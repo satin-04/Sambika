9532ba69331b6bba2fda68cb4d3e3158a7d7f6a6
Satin Jain <satin@eaiti.com>
2026-01-11 03:28:56 -0500

    redirect to home page on clicking sambika on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ function Navbar() {
     return ( 
         <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark p-2">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Sambika</a>
+                <a className="navbar-brand" href="/">Sambika</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
